a4eab575390c8d62641f9274f436bd0dbd9cf733
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2025-01-16 19:52:02 +0100

    ticketing application

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from . import db
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    qr_code = db.Column(db.Text, nullable=False)
