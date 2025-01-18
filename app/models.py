7439cf665964300e0159a71d84b591b2df1cb9ec
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2025-01-18 13:18:31 +0100

    modified the models.py file

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from . import db
 
-class Event(db.Model):
+class Event(db.Model): # database configuration
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
